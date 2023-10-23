91ee79f3cf4107a243c907b58326391692b0139d
younishamrh10 <yoha06001@utb.vaxjo.se>
2023-10-23 15:22:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Inlämningsuppgifter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hur många X per grupp? ");
+            int antalX = int.Parse(Console.ReadLine());
+
+            Console.Write("Hur många O per grupp? ");
+            int antalO = int.Parse(Console.ReadLine());
+
+            Console.Write("Hur många O-mönster per rad? ");
+            int antalOMonsterPerRad = int.Parse(Console.ReadLine());
+
+            Console.Write("Hur många rader? ");
+            int antalRader = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < antalRader; i++)
+            {
+                for (int j = 0; j < antalOMonsterPerRad; j++)
+                {
+                    for (int k = 0; k < antalX; k++)
+                    {
+                        Console.Write("X");
+                    }
+                    for (int k = 0; k < antalO; k++)
+                    {
+                        Console.Write("O");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
